207e3eda34a9e8edeab452aa9ece3fef3c08fe6c
raj-malasoni <raj.malasoni@impressico.com>
2024-04-29 09:16:58 -0400

    stale check main file updated

--- main.py
+++ main.py
@@ -72,7 +72,9 @@ try:
         for pull in pulls:
             print(f"hello from stale label:- {pull}")
             if "stale" in [label.name for label in pull.labels]:
+                print(f"hello from  label:-")
                 if time_diff > timedelta(days=msg.get("stale_close_days")):
+                    print(f"hello from  time-diff:- {time_diff}")
                     pull.edit(state="closed")
                     pull.create_issue_comment(msg.get("staled_PR_closing"))
                     GCHAT_MESSAGE.append(msg.get("staled_PR_closing"))
