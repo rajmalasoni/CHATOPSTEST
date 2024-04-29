c57f2546d5374b39b169e57e5647d89a6acbe703
raj-malasoni <raj.malasoni@impressico.com>
2024-04-29 09:19:14 -0400

    stale check main file updated

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ try:
         # 2. Close stalled PR if no activity for 2 days
         for pull in pulls:
             print(f"hello from stale label:- {pull}")
-            if "stale" in [label.name for label in pull.labels]:
+            if "Stale" in [label.name for label in pull.labels]:
                 print(f"hello from  label:-")
                 if time_diff > timedelta(days=msg.get("stale_close_days")):
                     print(f"hello from  time-diff:- {time_diff}")
